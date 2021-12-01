b0f239bfc9354afcdb607f492e06b4bafce8a418
unknown <edsnascimento89@gmail.com>
2021-11-30 21:58:50 -0300

    working data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 // SPA
 // SSR
 // SSG
+import {GetStaticProps} from 'next';
+import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR';
+// import { useEffect } from "react"
 
-import { useEffect } from "react"
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
 
-export default function Home(props) {
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   /*useEffect(() => {
     fetch('http://localhost:3333/episodes')
     .then(response => response.json())
@@ -30,9 +43,25 @@ export default function Home(props) {
 //   }
 // } USANDO SSR
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  //const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc') UTILIZANDO O FETCH
+  const {data} = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+    }
+  })
 
   return {
     props: {
